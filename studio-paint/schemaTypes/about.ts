aee43e80f2b53b655668b23364582e83f8de29e3
Adnan Lahrech <lahadn@gmail.com>
2026-03-12 22:56:41 +0100

    feat: made contact page paragraphs dynamic

--- studio-paint/schemaTypes/about.ts
+++ studio-paint/schemaTypes/about.ts
@@ -1,70 +1,82 @@
-import { defineField, defineType } from 'sanity'
+import {defineField, defineType} from 'sanity'
 
 export const aboutType = defineType({
-    name: 'about',
-    title: 'About',
-    type: 'document',
-    fields: [
-        defineField({
-            name: 'email',
-            type: 'string',
-            validation: (rule) => rule.required(),
-        }),
-        defineField({
-            name: 'address',
-            type: 'string'
-        }),
-        defineField({
-            name: 'phone',
-            type: 'string'
-        }),
-        defineField({
-            name: 'telegram',
-            type: 'string'
-        }),
-        defineField({
-            name: 'linkedin',
-            type: 'string'
-        }),
-        defineField({
-            name: 'instagram',
-            type: 'string'
-        }),
-        defineField({
-            name: 'facebook',
-            type: 'string'
-        }),
-        defineField({
-            name: 'aboutImage',
-            type: 'image',
-            validation: (rule) => rule.required(),
-        }),
-        defineField({
-            name: 'contactImage',
-            type: 'image'
-        }),
-        defineField({
-            name: 'photographyMessage',
-            type: 'string'
-        }),
-        defineField({
-            name: 'paintingMessage',
-            type: 'string'
-        }),
-        defineField({
-            name: 'collageMessage',
-            type: 'string'
-        }),
-        defineField({
-            name: 'aboutText',
-            type: 'array',
-            of: [{ type: 'block' }],
-        }),
-        defineField({
-            name: 'collections',
-            title: 'Collections',
-            type: 'array',
-            of: [{ type: 'reference', to: [{ type: 'collection' }] }],
-        }),
-    ],
+  name: 'about',
+  title: 'About',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'email',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'address',
+      type: 'string',
+    }),
+    defineField({
+      name: 'phone',
+      type: 'string',
+    }),
+    defineField({
+      name: 'telegram',
+      type: 'string',
+    }),
+    defineField({
+      name: 'linkedin',
+      type: 'string',
+    }),
+    defineField({
+      name: 'instagram',
+      type: 'string',
+    }),
+    defineField({
+      name: 'facebook',
+      type: 'string',
+    }),
+    defineField({
+      name: 'aboutImage',
+      type: 'image',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'contactImage',
+      type: 'image',
+    }),
+    defineField({
+      name: 'contactParagraph1',
+      type: 'string',
+    }),
+    defineField({
+      name: 'contactParagraph2',
+      type: 'string',
+    }),
+    defineField({
+      name: 'contactParagraph3',
+      type: 'string',
+    }),
+    defineField({
+      name: 'photographyMessage',
+      type: 'string',
+    }),
+    defineField({
+      name: 'paintingMessage',
+      type: 'string',
+    }),
+    defineField({
+      name: 'collageMessage',
+      type: 'string',
+    }),
+    defineField({
+      name: 'aboutText',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+    defineField({
+      name: 'collections',
+      title: 'Collections',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'collection'}]}],
+    }),
+  ],
 })
